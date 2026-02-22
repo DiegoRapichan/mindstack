c08162144d02d381fbe44e0f354cf5b2deb9b017
Diego Rapichan <direrapichan@gmail.com>
2026-02-22 17:42:26 -0300

    Apontando frontend para a API da nuvem

--- mindstack-frontend/src/services/api.ts
+++ mindstack-frontend/src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "http://localhost:3333",
+  baseURL: "https://mindstack-api-cdy3.onrender.com",
 });
 
 api.interceptors.request.use((config) => {
